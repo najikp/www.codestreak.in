c0d527dfb2631d3ec1eb13b27cdc16dcb0115180
Mohammed Jaseem Tp <56159302+Mohammedjaseem@users.noreply.github.com>
2022-09-17 12:18:05 +0530

    Fixing Js and CSS Not loading

---
+++ changeddir/static/chunks/1694-9b97c666334845fd.js
@@ -0,0 +1,327 @@
+"use strict";
+(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
+  [1694],
+  {
+    5247: function (e, s, i) {
+      var t = i(5893),
+        a = (i(7294), i(5675)),
+        n = i.n(a),
+        l = i(9583);
+      s.Z = function (e) {
+        var s = e.bgWhite;
+        return (0, t.jsx)("section", {
+          className: "feature-section-two ptb-120 ".concat(
+            s ? "bg-white" : "bg-light",
+            " "
+          ),
+          children: (0, t.jsx)("div", {
+            className: "container",
+            children: (0, t.jsxs)("div", {
+              className: "row align-items-center justify-content-center",
+              children: [
+                (0, t.jsx)("div", {
+                  className: "col-lg-6 col-md-12",
+                  children: (0, t.jsxs)("div", {
+                    className: "section-heading",
+                    children: [
+                      (0, t.jsx)("h4", {
+                        className: "h5 text-primary",
+                        children: "Our Values",
+                      }),
+                      (0, t.jsx)("h2", {
+                        children: "The Core Values that Drive Everything",
+                      }),
+                      (0, t.jsx)("p", {
+                        children:
+                          "Quickly incubate functional channels with multidisciplinary architectures. Authoritatively fabricate formulate exceptional innovation.",
+                      }),
+                      (0, t.jsxs)("ul", {
+                        className: "list-unstyled mt-5",
+                        children: [
+                          (0, t.jsxs)("li", {
+                            className: "d-flex align-items-start mb-4",
+                            children: [
+                              (0, t.jsx)("div", {
+                                className: "icon-box bg-primary rounded me-4",
+                                children: (0, t.jsx)("span", {
+                                  className: "fas text-white",
+                                  children: (0, t.jsx)(l.BRM, {
+                                    className: "fa-lg",
+                                  }),
+                                }),
+                              }),
+                              (0, t.jsxs)("div", {
+                                className: "icon-content",
+                                children: [
+                                  (0, t.jsx)("h3", {
+                                    className: "h5",
+                                    children: "Pixel Perfect Design",
+                                  }),
+                                  (0, t.jsx)("p", {
+                                    children:
+                                      "Progressively foster enterprise-wide systems whereas equity invested web-readiness harness installed.",
+                                  }),
+                                ],
+                              }),
+                            ],
+                          }),
+                          (0, t.jsxs)("li", {
+                            className: "d-flex align-items-start mb-4",
+                            children: [
+                              (0, t.jsx)("div", {
+                                className: "icon-box bg-danger rounded me-4",
+                                children: (0, t.jsx)("span", {
+                                  className: "fas text-white",
+                                  children: (0, t.jsx)(l.WIW, {
+                                    className: "fa-lg",
+                                  }),
+                                }),
+                              }),
+                              (0, t.jsxs)("div", {
+                                className: "icon-content",
+                                children: [
+                                  (0, t.jsx)("h3", {
+                                    className: "h5",
+                                    children: "Unique & Minimal Design",
+                                  }),
+                                  (0, t.jsx)("p", {
+                                    children:
+                                      "Dramatically administrate progressive metrics without error-free globally simplify standardized engineer efficient strategic.",
+                                  }),
+                                ],
+                              }),
+                            ],
+                          }),
+                          (0, t.jsxs)("li", {
+                            className: "d-flex align-items-start mb-4",
+                            children: [
+                              (0, t.jsx)("div", {
+                                className: "icon-box bg-dark rounded me-4",
+                                children: (0, t.jsx)("span", {
+                                  className: "fas text-white",
+                                  children: (0, t.jsx)(l.p4t, {
+                                    className: "fa-lg",
+                                  }),
+                                }),
+                              }),
+                              (0, t.jsxs)("div", {
+                                className: "icon-content",
+                                children: [
+                                  (0, t.jsx)("h3", {
+                                    className: "h5",
+                                    children: "Efficiency & Accountability",
+                                  }),
+                                  (0, t.jsx)("p", {
+                                    children:
+                                      "Objectively transition prospective collaboration and idea-sharing without focused maintain focused niche markets niches.",
+                                  }),
+                                ],
+                              }),
+                            ],
+                          }),
+                        ],
+                      }),
+                    ],
+                  }),
+                }),
+                (0, t.jsx)("div", {
+                  className: "col-lg-6 col-md-7",
+                  children: (0, t.jsxs)("div", {
+                    className:
+                      "feature-img-wrap position-relative d-flex flex-column align-items-end",
+                    children: [
+                      (0, t.jsxs)("ul", {
+                        className:
+                          "img-overlay-list list-unstyled position-absolute",
+                        children: [
+                          (0, t.jsxs)("li", {
+                            className:
+                              "d-flex align-items-center bg-white rounded shadow-sm p-3",
+                            children: [
+                              (0, t.jsx)(l.FJM, {
+                                className: "fas fa-2x me-2 text-primary mb-1",
+                              }),
+                              (0, t.jsx)("h6", {
+                                className: "mb-0",
+                                children: "Create a Free Account",
+                              }),
+                            ],
+                          }),
+                          (0, t.jsxs)("li", {
+                            className:
+                              "d-flex align-items-center bg-white rounded shadow-sm p-3",
+                            children: [
+                              (0, t.jsx)(l.FJM, {
+                                className: "fas fa-2x me-2 text-primary mb-1",
+                              }),
+                              (0, t.jsx)("h6", {
+                                className: "mb-0",
+                                children: "Install Our Tracking Pixel",
+                              }),
+                            ],
+                          }),
+                          (0, t.jsxs)("li", {
+                            className:
+                              "d-flex align-items-center bg-white rounded shadow-sm p-3",
+                            children: [
+                              (0, t.jsx)(l.FJM, {
+                                className: "fas fa-2x me-2 text-primary mb-1",
+                              }),
+                              (0, t.jsx)("h6", {
+                                className: "mb-0",
+                                children: "Start Tracking your Website",
+                              }),
+                            ],
+                          }),
+                        ],
+                      }),
+                      (0, t.jsx)(n(), {
+                        width: 493,
+                        height: 662,
+                        src: "/feature-img3.jpg",
+                        alt: "feature image",
+                        className: "img-fluid rounded-custom",
+                      }),
+                    ],
+                  }),
+                }),
+              ],
+            }),
+          }),
+        });
+      };
+    },
+    5487: function (e, s, i) {
+      var t = i(5893),
+        a = (i(7294), i(5675)),
+        n = i.n(a),
+        l = i(8494),
+        c = i(1911),
+        r = i(6381),
+        o = i(2095),
+        d = i(4762);
+      function m(e, s, i) {
+        return (
+          s in e
+            ? Object.defineProperty(e, s, {
+                value: i,
+                enumerable: !0,
+                configurable: !0,
+                writable: !0,
+              })
+            : (e[s] = i),
+          e
+        );
+      }
+      function h(e) {
+        for (var s = 1; s < arguments.length; s++) {
+          var i = null != arguments[s] ? arguments[s] : {},
+            t = Object.keys(i);
+          "function" === typeof Object.getOwnPropertySymbols &&
+            (t = t.concat(
+              Object.getOwnPropertySymbols(i).filter(function (e) {
+                return Object.getOwnPropertyDescriptor(i, e).enumerable;
+              })
+            )),
+            t.forEach(function (s) {
+              m(e, s, i[s]);
+            });
+        }
+        return e;
+      }
+      s.Z = function (e) {
+        var s = e.dark,
+          i = e.bgWhite;
+        return (0, t.jsx)(t.Fragment, {
+          children: (0, t.jsx)("section", {
+            className: "testimonial-section "
+              .concat(s ? "bg-dark" : "bg-light", " ptb-120 ")
+              .concat(i ? "bg-white" : ""),
+            children: (0, t.jsxs)("div", {
+              className: "container",
+              children: [
+                (0, t.jsx)("div", {
+                  className: "row justify-content-center align-content-center",
+                  children: (0, t.jsx)("div", {
+                    className: "col-md-10 col-lg-6",
+                    children: s
+                      ? (0, t.jsx)(o.Z, {
+                          subtitle: "Testimonial",
+                          title: "What They Say About Us",
+                          description:
+                            "Dynamically initiate market positioning total linkage with clicks-and-mortar technology compelling data for cutting-edge markets.",
+                          darkBg: !0,
+                          centerAlign: !0,
+                        })
+                      : (0, t.jsx)(o.Z, {
+                          subtitle: "Testimonial",
+                          title: "What They Say About Us",
+                          description:
+                            "Dynamically initiate market positioning total linkage with clicks-and-mortar technology compelling data for cutting-edge markets.",
+                          centerAlign: !0,
+                        }),
+                  }),
+                }),
+                (0, t.jsx)("div", {
+                  className: "row",
+                  children: (0, t.jsx)("div", {
+                    className: "col-12",
+                    children: (0, t.jsxs)("div", {
+                      className: "position-relative",
+                      children: [
+                        (0, t.jsx)(
+                          l.tq,
+                          h(
+                            {},
+                            {
+                              slidesPerView: 2,
+                              mousewheel: !0,
+                              spaceBetween: 30,
+                              slidesPerGroup: 2,
+                              loop: !0,
+                              navigation: {
+                                nextEl:
+                                  ".swiper-nav-control .swiper-button-next",
+                                prevEl:
+                                  ".swiper-nav-control .swiper-button-prev",
+                              },
+                              breakpoints: {
+                                320: { slidesPerView: 1 },
+                                640: { slidesPerView: 1 },
+                                768: { slidesPerView: 2, spaceBetween: 20 },
+                                1024: { slidesPerView: 2, spaceBetween: 25 },
+                                1142: { slidesPerView: 2, spaceBetween: 30 },
+                              },
+                            },
+                            {
+                              modules: [c.W_],
+                              children: d.rZ.map(function (e) {
+                                return (0,
+                                t.jsx)(l.o5, { children: (0, t.jsxs)("div", { className: "p-5 rounded-custom position-relative ".concat(s ? "bg-custom-light text-white" : "border border-2", "   "), children: [(0, t.jsx)("img", { src: "/testimonial/quotes-dot.svg", alt: "quotes", width: "100", className: "position-absolute left-0 top-0 z--1 p-3" }), (0, t.jsxs)("div", { className: "d-flex mb-32 align-items-center", children: [(0, t.jsx)(n(), { width: 60, height: 60, src: e.authorImg, className: "img-fluid rounded", alt: "user" }), (0, t.jsxs)("div", { className: "author-info ms-3", children: [(0, t.jsx)("h6", { className: "mb-0", children: e.authorName }), (0, t.jsx)("small", { children: e.authorTitle })] })] }), (0, t.jsxs)("blockquote", { children: [(0, t.jsx)("h6", { children: e.quoteTitle }), e.authorQuote] }), (0, t.jsx)(r.Z, {})] }) }, e.id);
+                              }),
+                            }
+                          )
+                        ),
+                        (0, t.jsxs)("div", {
+                          className: "swiper-nav-control",
+                          children: [
+                            (0, t.jsx)("span", {
+                              className: "swiper-button-next",
+                            }),
+                            (0, t.jsx)("span", {
+                              className: "swiper-button-prev",
+                            }),
+                          ],
+                        }),
+                      ],
+                    }),
+                  }),
+                }),
+              ],
+            }),
+          }),
+        });
+      };
+    },
+  },
+]);
